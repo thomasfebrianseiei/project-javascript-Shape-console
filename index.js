7d02201e900edcbb013e731628a5ce7d301c6c74
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2019-09-03 12:54:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// The formula of rectangle area is
+// Area = width * length
+const rectangleWidth = 100
+const rectangleLength = 20
+const number1=2
+const number2=5
+
+const rectangleArea = rectangleWidth * rectangleLength
+const pangkat= number1 ** number2
+const akar= pangkat**0.5
+
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength} true`)
+console.log(`${pangkat} = ${number1} pangkat ${number2} true`)
+console.log(`${akar} = akar ${pangkat} true`)
+
